4a50cca63d7d3f4082386183be971013aab025ba
Hernando Abella <hernandoabella@gmail.com>
2023-06-23 15:04:20 -0500

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    for (let i= 1; i <= 41; i++) {
+        let line = document.getElementsByClassName("line" + i)[0];
+        line.style.visibility = "visible";
+    }
+}
+    
